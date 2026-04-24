f1606cf085a82de115331a41a228674cdf654246
عبدالرحمن محمد زيدان محمد <abdallrahmanzidan@fsc.bu.edu.eg>
2026-04-24 22:50:05 +0300

    Add utility classes for app colors and asset paths
    - Created AppColor class to define primary, secondary, and text colors.
    - Added AssetsImage class for image asset paths used in onboarding screens.
    - Introduced AssetsIcon class for icon asset paths including eye, flag, logout, send, and timer icons.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primaryColor = Color(0xff5F33E1);
+  static const Color secondaryColor = Color(0xFFE6E6E6);
+  static const Color textColor = Color(0xff24252C);
+ 
+}
---
+++ lib/core/utils/assets_icons.dart
@@ -0,0 +1,6 @@
+abstract class AssetsImage {
+  static const String emptyHomeScreen = "assets/images/empty_tasks_screen.png";
+  static const String onboadingFirst = "assets/images/Onboadingf_first.png";
+  static const String onboadingSecond = "assets/images/Onboading_second.png";
+  static const String onboadingThird = "assets/images/Onboading_third.png";
+}
